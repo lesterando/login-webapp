e1ef7b23753a0126d7c1e1eea450ae6f9001329f
Lstr Andztr <lester.ando1993@gmail.com>
2018-07-15 21:10:27 +0800

    new code for Logo.js [converting class to stateless functional component]

--- login-app/src/Logo.js
+++ login-app/src/Logo.js
@@ -2,19 +2,14 @@ import React, { Component } from 'react';
 import { Image } from 'react-bootstrap';
 import './Logo.css';
 
-class Logo extends Component {
-
-    render() {
-        return (
-          <div className="container">
-            <div className="Logo">
-                <div className="App-header">
-                    <Image src="../logo.png" className="center" alt="logo" responsive />
-                </div>
-            </div>
-          </div>
-        )
-    }
-}
+const Logo = () =>(
+  <div className="container">
+    <div className="Logo">
+        <div className="App-header">
+            <Image src="../logo.png" className="center" alt="logo" responsive />
+        </div>
+    </div>
+  </div>
+);
 
 export default Logo;
